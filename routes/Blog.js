c385c1ba6bd47b9afa7016d03fc2e5ebb5206661
D-coder <chuksjoshuaa@gmail.com>
2022-10-31 20:00:47 +0100

    Updated Changes

--- routes/Blog.js
+++ routes/Blog.js
@@ -5,6 +5,7 @@ import {
   createBlog,
   updateDraftToPublishedBlog,
   editPublishedBlog,
+  editDraftBlog,
 } from "../controllers/Blog.js";
 
 const router = express();
@@ -14,5 +15,6 @@ router.get("/draft", getAllDraftBlogs);
 router.post("/create", createBlog);
 router.patch("/draft_to_publish/update/:id", updateDraftToPublishedBlog);
 router.patch("/edit/published/:id", editPublishedBlog);
+router.patch("/edit/draft/:id", editDraftBlog);
 
 export default router;
