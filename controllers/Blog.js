71e2c71cc76ce4ca6dc3a18305d22345fd9aa3e1
Chuks Joshua <chuksmbanasoj@gmail.com>
2022-11-01 14:35:12 +0100

    WIP: Added search functionality to blog

--- controllers/Blog.js
+++ controllers/Blog.js
@@ -6,25 +6,58 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
-//Get all published blogs
+//Get all published blogs that can be accessed by both logged in and logged out users
 export const getAllPublishedBlogs = async (req, res) => {
+  const { page } = req.query;
+  const LIMIT = 20;
+  const startIndex = (Number(page) - 1) * LIMIT;
+  const total = await Blog.countDocuments({});
   try {
     const publishedBlog = await Blog.find({})
+      .sort({ read_count: -1, reading_time: -1, timestamp: -1 })
+      .limit(LIMIT)
+      .skip(startIndex)
       .where("state")
       .equals("published");
-    res.status(StatusCodes.OK).json({ data: publishedBlog });
+    res.status(StatusCodes.OK).json({
+      data: publishedBlog,
+      currentPage: Number(page),
+      NumberOfPages: Math.ceil(total / LIMIT),
+    });
   } catch (error) {
     res.status(StatusCodes.BAD_REQUEST).json({ msg: "No published blogs" });
   }
 };
 
-//Get all draft blogs
-export const getAllDraftBlogs = async (req, res) => {
+//Get all searchable published blog that can be accessed by both logged in and logged out users
+export const getAllSearchPublishedBlogs = async (req, res) => {
+  const { searchQuery, tags, user } = req.query;
+
+  const title = new RegExp(searchQuery, "i");
+  const author = new RegExp(user, "i");
+  const blogs = await Blog.find({
+    $or: [{ title }, { tags: { $in: tags.split(",") } }, { author }],
+  })
+    .where("state")
+    .equals("published");
+  res.status(200).json({
+    data: blogs,
+  });
+};
+
+//get single publish blog
+export const getSinglePublishBlog = async (req, res) => {
+  const { id: _id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(StatusCodes.NOT_FOUND).send(`No blog with id: ${_id}`);
+  }
+
   try {
-    const draftBlog = await Blog.find({}).where("state").equals("draft");
-    res.status(StatusCodes.OK).json({ data: draftBlog });
+    const blog = await Blog.findById(_id).where("state").equals("published");
+    res.status(200).json({ blog });
   } catch (error) {
-    res.status(StatusCodes.BAD_REQUEST).json({ msg: "No draft blogs" });
+    res.status(404).json({ msg: "No blog matches this" });
   }
 };
 
@@ -69,6 +102,16 @@ export const createBlog = async (req, res) => {
     res
       .status(StatusCodes.CONFLICT)
       .json({ msg: "New blog cannot be created. try again" });
+  }
+};
+
+//Get all draft blogs
+export const getAllDraftBlogs = async (req, res) => {
+  try {
+    const draftBlog = await Blog.find({}).where("state").equals("draft");
+    res.status(StatusCodes.OK).json({ data: draftBlog });
+  } catch (error) {
+    res.status(StatusCodes.BAD_REQUEST).json({ msg: "No draft blogs" });
   }
 };
 
@@ -154,5 +197,67 @@ export const editPublishedBlog = async (req, res) => {
       .where("state")
       .equals("published");
     res.status(200).json(updatedBlog);
+  }
+};
+
+//Delete draft blog
+export const deleteDraftBlog = async (req, res) => {
+  const { id: _id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(StatusCodes.BAD_REQUEST).send(`No blog with id: ${_id}`);
+  } else {
+    await Blog.findByIdAndDelete(_id).where("state").equals("draft");
+  }
+  res.status(StatusCodes.OK).send("Draft blog was deleted Successfully");
+};
+
+//Delete published blog
+export const deletePublishedBlog = async (req, res) => {
+  const { id: _id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(StatusCodes.BAD_REQUEST).send(`No blog with id: ${_id}`);
+  } else {
+    await Blog.findByIdAndDelete(_id).where("state").equals("published");
+  }
+  res.status(StatusCodes.OK).send("Draft blog was deleted Successfully");
+};
+
+//Get specific author blog
+export const getUserBlogs = async (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    return res.status(401).json({ msg: "Authentication is not correct" });
+  }
+  const token = authHeader.split(" ")[1];
+
+  let decodedData = "";
+  if (token !== undefined || token !== null) {
+    decodedData = jwt.verify(token, process.env.JWT_SECRET);
+    decodedData = decodedData?.id;
+  }
+
+  const { page } = req.query;
+  const LIMIT = 20;
+  const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of the page
+  const total = await Blog.countDocuments({});
+
+  try {
+    if (decodedData === undefined || decodedData === null) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ msg: "Invalid User or no user signed in yet" });
+    }
+    const blogData = await Blog.find({ author: decodedData })
+      .limit(LIMIT)
+      .skip(startIndex);
+    res.status(StatusCodes.OK).json({
+      data: blogData,
+      currentPage: Number(page),
+      NumberOfPages: Math.ceil(total / LIMIT),
+    });
+  } catch (error) {
+    res.status(StatusCodes.BAD_REQUEST).json({ msg: "No available blogs" });
   }
 };
--- routes/Blog.js
+++ routes/Blog.js
@@ -6,15 +6,25 @@ import {
   updateDraftToPublishedBlog,
   editPublishedBlog,
   editDraftBlog,
+  getUserBlogs,
+  deleteDraftBlog,
+  deletePublishedBlog,
+  getAllSearchPublishedBlogs,
+  getSinglePublishBlog,
 } from "../controllers/Blog.js";
 
 const router = express();
 
 router.get("/published", getAllPublishedBlogs);
+router.get("/published/:id", getSinglePublishBlog);
+router.get("/search", getAllSearchPublishedBlogs);
+router.get("/all_user_blogs", getUserBlogs);
 router.get("/draft", getAllDraftBlogs);
 router.post("/create", createBlog);
 router.patch("/draft_to_publish/update/:id", updateDraftToPublishedBlog);
 router.patch("/edit/published/:id", editPublishedBlog);
 router.patch("/edit/draft/:id", editDraftBlog);
+router.delete("/delete/draft/:id", deleteDraftBlog);
+router.delete("/delete/published/:id", deletePublishedBlog);
 
 export default router;
