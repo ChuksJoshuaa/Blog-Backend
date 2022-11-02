9e4e9fcc8f07abad26d200d57f6cbfc09f99dc3b
Chuks Joshua <chuksmbanasoj@gmail.com>
2022-11-02 08:48:01 +0100

    Updated Changes

--- routes/Blog.js
+++ routes/Blog.js
@@ -11,19 +11,28 @@ import {
   deletePublishedBlog,
   getAllSearchPublishedBlogs,
   getSinglePublishBlog,
+  blogReadCount,
 } from "../controllers/Blog.js";
 
 const router = express();
 
+//Get requests
 router.get("/published", getAllPublishedBlogs);
 router.get("/published/:id", getSinglePublishBlog);
 router.get("/search", getAllSearchPublishedBlogs);
 router.get("/all_user_blogs", getUserBlogs);
 router.get("/draft", getAllDraftBlogs);
+
+//Post requests
 router.post("/create", createBlog);
+
+//Patch requests
 router.patch("/draft_to_publish/update/:id", updateDraftToPublishedBlog);
 router.patch("/edit/published/:id", editPublishedBlog);
 router.patch("/edit/draft/:id", editDraftBlog);
+router.patch("/read_count/:id", blogReadCount);
+
+//Delete requests
 router.delete("/delete/draft/:id", deleteDraftBlog);
 router.delete("/delete/published/:id", deletePublishedBlog);
 
